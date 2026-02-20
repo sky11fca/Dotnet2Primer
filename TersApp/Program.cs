58b2041c4c7ad79cfb8b7e1736a87fc8635993ce
mrbogdanovich <bogdan.bzn03@protonmail.com>
2026-02-20 07:56:12 +0200

    Test Project

---
+++ TersApp/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text.Json.Serialization;
+using TersApp.Model;
+
+Console.WriteLine("Hello, World!");
+
+var greeter = new Greeter(Guid.NewGuid(), "Begin .NET act II", 200);
+
+Console.WriteLine(greeter);
